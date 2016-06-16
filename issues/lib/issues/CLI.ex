7e908ade332adeda2a9a9aa75035b0d5951d9bcd
Alessandro Di Maria <adm@m42.ch>
2016-06-16 22:21:06 +0200

    CLI parse

--- issues/lib/issues/CLI.ex
+++ issues/lib/issues/CLI.ex
@@ -1,28 +1,30 @@
-defmodule CLI do
+defmodule Issues.CLI do
+
+  @default_count 5
 
   def run(argv) do
-    parse(OptionParser.parse(argv, switches: [help: :boolean], aliases: [:h, :help] ))
+    argv
+    |> OptionParser.parse(switches: [help: :boolean], aliases: [h: :help] )
+    |> parse_args
+    |> fetch
   end
 
-  def parse([help: true], _ , _ ) do
-    IO.puts "help me!"
+  def parse_args({[help: true], _, []}) do
+    IO.puts "will fetch issues from a github repo"
+    IO.puts "usage: issues user project <count>"
+    :help
   end
 
-  def parse(_, [user, project, count], _) do
-    IO.puts "got #{user} #{project} #{count}"
+  def parse_args({[], [user, project, count], []}) do
+    {user, project, String.to_integer(count)}
   end
 
-  def parse(_, [user, project], _) do
-    IO.puts "got #{user} #{project}"
+  def parse_args({[], [user, project], []}) do
+    {user, project, @default_count}
   end
 
-  def parse(_, _, error) do
-    IO.puts "error #{error}"
-    IO.puts "usage: myapp user project <count>"
-  end
+  def fetch({user, project, count}) do
 
-  def parse(one) do
-    IO.puts "One: #{one}"
   end
 
 end
---
+++ issues/test/issues/cli_test.exs
@@ -0,0 +1,16 @@
+defmodule CLITest do
+  use ExUnit.Case
+  doctest Issues
+
+  import Issues.CLI, only: [ run: 1 ]
+
+  test "parse argv with help" do
+    assert Issues.CLI.parse_args({[help: true], [], []}) == :help
+  end
+
+  test "parse argv with project and user" do
+    assert Issues.CLI.parse_args({[], ["grrrisu", "dawning", "100"], []}) == {"grrrisu", "dawning", 100}
+    assert Issues.CLI.parse_args({[], ["grrrisu", "dawning"], []})        == {"grrrisu", "dawning", 5}
+  end
+
+end

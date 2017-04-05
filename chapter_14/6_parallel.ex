27c83663f3c4bfc6cc0ce77df9f30cf7b72f4638
Alessandro Di Maria <adm@m42.ch>
2017-04-05 21:48:50 +0200

    comments on parallel.ex

--- chapter_14/6_parallel.ex
+++ chapter_14/6_parallel.ex
@@ -1,14 +1,14 @@
 defmodule Parallel do
 
   def pmap(collection, func) do
-    me = self()
+    me = self() # send all results back to me (first process)
     collection
     |> Enum.map(fn (element) ->
         spawn_link fn -> (send me, { self(), func.(element)}) end
       end)
     |> Enum.map(fn (pid) ->
         receive do
-          {^pid, result} -> result
+          {^pid, result} -> result # ^pid keeps results in order
         end
       end)
   end
